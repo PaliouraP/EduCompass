12c48f095b3f8c88ad1e2acffdf44e45565cacdd
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-06-24 01:28:02 +0300

    annotations in User model

--- EduCompass/EduCompass/Models/User.cs
+++ EduCompass/EduCompass/Models/User.cs
@@ -1,13 +1,29 @@
-﻿namespace EduCompass.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduCompass.Models
 {
     public class User
     {
+        // PRIMARY KEY
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
         public int Semester { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
     }
