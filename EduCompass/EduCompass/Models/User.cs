95e42e42c926ba50434b60d4ea6b3a54e6951a02
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-06-24 01:25:49 +0300

    user model initialization

---
+++ EduCompass/EduCompass/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace EduCompass.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Semester { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
