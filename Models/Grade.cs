8da7cc6597c65274689108a9975adb235fd4a062
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-08-17 17:28:47 +0300

    grade schema

--- Models/Grade.cs
+++ Models/Grade.cs
@@ -1,9 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EduCompass.Models;
 
 public class Grade
 {
+    // PRIMARY KEY
     [Key]
-    public int Id
+    public int Id { get; set; }
+    
+    [ForeignKey("User"), NotNull]
+    public int UserId { get; set; }
+    
+    [ForeignKey("Course"), NotNull]
+    public int CourseId { get; set; }
+    
+    public int FinalGrade { get; set; }
+    
+    public int InterestScore { get; set; }
+    
+    public DateTime Created { get; set; } = DateTime.Now;
+    
+    // Navigation Properties
+    public virtual User _User { get; set; }
+    
+    public virtual Course _Course { get; set; }
+    
 }
