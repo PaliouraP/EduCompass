fcdfa5f069243e4d6cf2afea3698ef8110623510
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-09-04 17:30:18 +0300

    intro controller added

---
+++ Controllers/IntroController.cs
@@ -0,0 +1,35 @@
+﻿using EduCompass.Data;
+using EduCompass.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EduCompass.Controllers;
+
+public class IntroController : Controller
+{
+    private readonly ILogger<IntroController> _logger;
+    private readonly ApplicationDbContext _database;
+    private User _currentUser;
+
+    public IntroController(ILogger<IntroController> logger, ApplicationDbContext database)
+    {
+        _logger = logger;
+        _database = database;
+    }
+    
+    // this method gets executed before any views.
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        // if the session exists, do nothing.
+        if (string.IsNullOrEmpty(HttpContext.Session.GetString("username")))
+        {
+            // if it doesn't, then log the user out.
+            TempData["Error"] = "Your session has expired. Please log in again.";
+            context.Result = RedirectToAction("Login", "Auth");
+            base.OnActionExecuting(context);
+            return;
+        }
+
+        _currentUser = _database.Users.First(u => u.Username == HttpContext.Session.GetString("username"));
+    }
+}
